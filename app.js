775e618fab31b1844cccc86ef5e05df855ee8a2b
thebrownone <97177514+thebrownone@users.noreply.github.com>
2023-02-02 12:29:31 -0500

    added the ability to edit previous notes

--- app.js
+++ app.js
@@ -2,14 +2,23 @@
 class App {
   constructor() {
     this.notes = [];
+    this.title = '';
+    this.text = '';
+    this.id = '';
 
+    this.$placeholder = document.querySelector('#placeholder');
     this.$form = document.querySelector('#form');
+    this.$notes = document.querySelector('#notes');
     this.$noteTitle = document.querySelector('#note-title');
     this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector('#form-buttons');
-    this.$placeholder = document.querySelector('#placeholder')
-    this.$notesDiv = document.querySelector('#notes')
-    this.$formCloseButton = document.querySelector("#form-close-button")
+    this.$formCloseButton = document.querySelector("#form-close-button");
+    this.$modal = document.querySelector('.modal');
+    this.$modalTitle = document.querySelector('.modal-title');
+    this.$modalText = document.querySelector('.modal-text');
+    this.$modalCloseButton = document.querySelector('.modal-close-button')
+    
+
     
     this.addEventListeners();
 
@@ -18,6 +27,8 @@ class App {
   addEventListeners () {
     document.body.addEventListener('click', event => {
       this.handleFormClick(event);
+      this.selectNote(event);
+      this.openModal(event);
     })  
     this.$form.addEventListener('submit', event => {
       console.log(event)
@@ -27,12 +38,16 @@ class App {
       const hasNote = title || text;
 
       if(hasNote) {
-        this.addNote({title, text})
+        this.addNote({title, text});
       }
     })
     this.$formCloseButton.addEventListener('click', event => {
       event.stopPropagation();
       this.closeForm();
+    })
+
+    this.$modalCloseButton.addEventListener('click', event => {
+      this.closeModal(event);
     })
   }
 
@@ -66,6 +81,20 @@ class App {
     this.$noteText.value = "";
   }
 
+  openModal(event) {
+    if (event.target.closest('.note')) {
+      this.$modal.classList.toggle('open-modal');
+      this.$modalTitle.value = this.title;
+      this.$modalText.value = this.text;
+    }
+  }
+
+  closeModal(event) {
+    this.editNote();
+    this.$modal.classList.toggle('open-modal');
+  }
+
+
   addNote({title, text}) {
     const newNote = {
       title,
@@ -78,24 +107,45 @@ class App {
     this.closeForm();
   }
 
-    displayNotes() {
-      const hasNotes = this.notes.length > 0;
-      this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
-      
-      this.$notesDiv.innerHTML = this.notes.map(note => `
-        <div style="background: ${note.color}" class= "note">
-          <div class="${note.title && 'note-title'}">${note.title}</div>
-          <div class= "note-text">${note.text}</div>
-          <div class="toolbar-container">
-            <div class="toolbar">
-            <img class="toolbar-color" src="https://icon.now.sh/palette">
-            <img class="toolbar-delete" src="https://icon.now.sh/delete">
-            </div>
-          </div>
+  editNote(){
+    const title = this.$modalTitle.value;
+    const text = this.$modalText.value;
+    this.notes = this.notes.map( note =>
+      note.id === Number(this.id) ? {...note, title, text} : note
+    );
+    
+    this.displayNotes();
+  }
 
+  selectNote(event){
+    const $selectedNote = event.target.closest('.note');
+    if(!$selectedNote) return;
+    //here we are trying to get a children without having selected a children 
+    //so we return in the above step if selected note isnt present so the rest of the code doesnt run.
+    const [$noteTitle, $noteText] = $selectedNote.children;
+    this.title = $noteTitle.innerText;
+    this.text = $noteText.innerText;
+    this.id = $selectedNote.dataset.id;
+  }
+
+  displayNotes() {
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+    
+    this.$notes.innerHTML = this.notes.map(note => `
+      <div style="background: ${note.color}" class= "note" data-id= "${note.id}">
+        <div class="${note.title && 'note-title'}">${note.title}</div>
+        <div class= "note-text">${note.text}</div>
+        <div class="toolbar-container">
+          <div class="toolbar">
+          <img class="toolbar-color" src="https://icon.now.sh/palette">
+          <img class="toolbar-delete" src="https://icon.now.sh/delete">
+          </div>
         </div>
-        `).join("")
-    }
+
+      </div>
+      `).join("")
+  }
 
 }
 
